ad5721de6e3396f27a75428baaacf1073f73defe
Ole Begemann <ole@oleb.net>
2024-09-30 14:34:35 +0200

    Model Game as an array of moves, remove side-effects from game logic code

--- Sources/SetSolver/Game.swift
+++ Sources/SetSolver/Game.swift
@@ -4,56 +4,70 @@
 /// Models a one-player game where the goal is to find valid sets until the card
 /// deck is exhausted. This does not model a multi-player game where scores are
 /// kept by player.
-public struct Game {
+public struct Game: Hashable, Sendable {
     public var remainingCards: [Card]
     public var openCards: Set<Card> = []
-    public var foundSets: [Set<Card>] = []
-    public var moveNumber: Int = 1
+    public var moves: [Move] = []
 
     public init() {
         self.remainingCards = Card.allCases
     }
 
+    public var foundSets: [Set<Card>] {
+        moves.compactMap(\.foundSet)
+    }
+
+    /// Starts a new game. This shuffles the deck.
+    ///
+    /// - TODO: Add the ability to start the game with a deterministic card
+    ///   order in the deck, for reproducible games.
     public mutating func start() {
         remainingCards = Card.allCases.shuffled()
         openCards = Set(remainingCards.suffix(12))
         remainingCards.removeLast(12)
-        foundSets = []
-        moveNumber = 1
+        moves = []
     }
 
-    public mutating func makeMove() -> Bool {
-        print("Move \(moveNumber)")
-        defer {
-            moveNumber += 1
+    /// Executes a single move.
+    ///
+    /// - Returns: The executed move, or `nil` if there are no moves left
+    ///   (game over).
+    public mutating func makeMove() -> Move? {
+        let foundSet = findSets(cards: openCards).first
+        let isGameOver = foundSet == nil && remainingCards.isEmpty
+        guard !isGameOver else {
+            return nil
         }
 
-        print("Open cards:", openCards.map(\.notation).joined(separator: ", "))
-        let sets = findSets(cards: openCards)
-        if let foundSet = sets.first {
-            print("Found set \(foundSet.map(\.notation).joined(separator: ", "))")
-            foundSets.append(foundSet)
-            openCards.formSymmetricDifference(foundSet)
-        } else {
-            print("No set found")
-            if remainingCards.isEmpty {
-                // Game over
-                return false
-            }
+        var move = Move(foundSet: foundSet, newCards: nil)
+        defer {
+            moves.append(move)
         }
+        if let foundSet {
+            openCards.formSymmetricDifference(foundSet)
+        }
+
         // Deal 3 new cards
-        let didNotFindSet = sets.isEmpty
+        let didNotFindSet = foundSet == nil
         if !remainingCards.isEmpty {
             if openCards.count <= 9 || didNotFindSet {
-                let newCards = remainingCards.suffix(3)
-                print("Dealing new cards: \(newCards.map(\.notation).joined(separator: ", "))")
+                let newCards = Set(remainingCards.suffix(3))
+                move.newCards = newCards
                 openCards.formUnion(newCards)
                 remainingCards.removeLast(newCards.count)
-            } else {
-                print("We already have \(openCards.count) open cards, not dealing new cards")
             }
         }
-        print("---")
-        return true
+
+        return move
+    }
+}
+
+public struct Move: Hashable, Sendable {
+    public var foundSet: Optional<Set<Card>>
+    public var newCards: Optional<Set<Card>>
+
+    public init(foundSet: Set<Card>?, newCards: Set<Card>?) {
+        self.foundSet = foundSet
+        self.newCards = newCards
     }
 }
