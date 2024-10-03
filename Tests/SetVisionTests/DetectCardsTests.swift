25627e40b538c65a22d2b8b45ca65c2821a49505
Ole Begemann <ole@oleb.net>
2024-10-03 18:15:30 +0200

    Expand vision test to all test images (many are failing)

--- Tests/SetVisionTests/DetectCardsTests.swift
+++ Tests/SetVisionTests/DetectCardsTests.swift
@@ -2,10 +2,20 @@ import SetVision
 import Testing
 
 struct DetectCardsTests {
-    @Test func findRectangles() async throws {
-        let imageURL = try #require(fixtureURL("set-cards-1.jpeg"))
+    @Test(arguments: [
+        (filename: "set-cards-1.jpeg", expect: 9),
+        (filename: "set-cards-2.jpeg", expect: 9),
+        (filename: "set-cards-3.jpeg", expect: 9),
+        (filename: "set-cards-4.jpeg", expect: 9),
+        (filename: "set-cards-5.jpeg", expect: 9),
+        (filename: "set-cards-6.jpeg", expect: 9),
+        (filename: "set-cards-7.jpeg", expect: 9),
+        (filename: "set-cards-8.jpeg", expect: 9),
+        (filename: "set-cards-9.jpeg", expect: 9),
+    ])
+    func findRectangles(imageFilename: String, expectedRectCount: Int) async throws {
+        let imageURL = try #require(fixtureURL(imageFilename))
         let rectangles = try await detectRectangles(in: imageURL)
-        #expect(rectangles.count == 9)
+        #expect(rectangles.count == expectedRectCount, "\(imageFilename)")
     }
-
 }
